dda2d5cd30bdba6843de2459c035421e29128bd6
iustinian <o.iustin@gmail.com>
2024-08-12 09:25:41 +0200

    Cleare api data

--- Cvr.Dev.Client/Program.cs
+++ Cvr.Dev.Client/Program.cs
@@ -14,7 +14,7 @@ namespace Example
             Configuration config = new Configuration();
             config.BasePath = "https://api.cvr.dev";
             // Configure API key authorization: APIKey
-            config.ApiKey.Add("Authorization", "cvr.dev_8fdb7539213bf2085b5ebaba886814c3");
+            config.ApiKey.Add("Authorization", "Your API Key");
             // Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
             // config.ApiKeyPrefix.Add("Authorization", "Bearer");
 
@@ -22,14 +22,8 @@ namespace Example
 
             try
             {
-                // var pathParams = new Dictionary<string, string>()
-                // {
-                //     {"cvr_number", "38495224"},
-                //     {"navn", "Quadsat"}
-                // };
-                // var result = apiInstance.Client.Get<Virksomhed>("https://api.cvr.dev/api/cvr/virksomhed", new RequestOptions(){PathParameters = pathParams}, config);
-                var client = new RDataFraCVRApi(config); // TestAPIKey |
-                List<Virksomhed> result = client.GetCvrVirksomhed("38495224", "Quadsat");
+                // Test API key
+                var result = apiInstance.TestAPIKey();
                 Console.WriteLine(result);
             }
             catch (ApiException e)
